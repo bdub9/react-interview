4b8dd1e0bf2fef99f64a2d11df65880006a0e9b7
Dima Doronin <doronindm@gmail.com>
2020-08-22 13:54:11 -0400

    React boilerplate based on CRA

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="react" className="logo"/> React Interview
+    </div>
+  );
+}
+
+export default App;
